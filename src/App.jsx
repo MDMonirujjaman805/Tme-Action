ced61cd59a76293c014ef4a27f928eb28a380cbd
MD Monirujjaman <mdmonirujjaman@MDs-MacBook-Air.local>
2025-07-28 12:00:40 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import DaisyNav from './components/DaisyNav'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+   <div>
+    <DaisyNav></DaisyNav>
+    <Navbar></Navbar>
+   </div>
+  )
+}
+
+export default App
